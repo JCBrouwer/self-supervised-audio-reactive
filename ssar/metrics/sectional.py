02c31a80aaa469065782c4a14436b0e1dddc7383
Hans <hans@brouwer.work>
2022-03-23 11:13:01 +0000

    much faster loading

--- ssar/metrics/sectional.py
+++ ssar/metrics/sectional.py
@@ -2,32 +2,27 @@ from glob import glob
 from time import time
 
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
-import tvl
 from sklearn.utils import shuffle
 from ssar.metrics.chroma import chromatic_reactivity
 from ssar.metrics.rhythmic import rhythmic_reactivity
-from torchaudio.prototype.io import Streamer
+import decord as de
+
+de.bridge.set_bridge("torch")
 
 if __name__ == "__main__":
     for video_file in shuffle(glob("/home/hans/datasets/audiovisual/maua/*")):
         print("loading:", video_file)
         t = time()
-        streamer = Streamer(src=video_file)
-        fps = 24
-        sr = 1024 * fps
-        info = streamer.get_src_stream_info(0)
-        w, h = info.width, info.height
-        streamer.add_basic_audio_stream(frames_per_chunk=round(sr), sample_rate=sr)
-        streamer.add_basic_video_stream(frames_per_chunk=round(fps), width=w // 4, height=h // 4, frame_rate=fps)
-        audio, video = [], []
-        for audio_chunk, video_chunk in streamer.stream():
-            audio.append(audio_chunk.mean(1).cuda(non_blocking=True))
-            video.append(video_chunk.div(255.0).cuda(non_blocking=True))
-        audio, video = torch.cat(audio), torch.cat(video)
+        v = de.VideoReader(video_file)
+        fps = v.get_avg_fps()
+        h, w, c = v[0].shape
+        sr = round(1024 * fps)
+        av = de.AVReader(video_file, sample_rate=sr, width=w // 4, height=h // 4)
+        audio, video = av[:]
+        audio = torch.cat(audio, dim=1).squeeze().float().cuda()
+        video = video.permute(0, 3, 1, 2).float().cuda()
         print("took:", time() - t)
-
         print("rhythmic reactivity:", rhythmic_reactivity(audio, sr, video, fps).item())
         print("chromatic reactivity:", chromatic_reactivity(audio, sr, video, fps).item())
         print()
